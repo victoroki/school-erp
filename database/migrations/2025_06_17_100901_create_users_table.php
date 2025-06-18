b9a15b4915e1a96231270d9ea401dfaf40559451
mongarevic <mongarevictorok@gmail.com>
2025-06-18 12:34:26 +0300

    Update 2025_06_17_100901_create_users_table.php

--- database/migrations/2025_06_17_100901_create_users_table.php
+++ database/migrations/2025_06_17_100901_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name', 50)->unique('username');
             $table->string('password');
             $table->string('email', 100)->unique('email');
-            $table->enum('user_type', ['admin', 'teacher', 'student', 'parent', 'staff']);
+            $table->enum('user_type', ['admin', 'teacher', 'student', 'parent', 'staff'])->default('student'); // ✅ Set default
             $table->boolean('is_active')->nullable()->default(true);
             $table->dateTime('email_verified_at')->nullable();
             $table->dateTime('last_login')->nullable();
